e1d800ad8aee461c6ab6396730591c56530f14cc
megasxgigavolt <63855153+megasxgigavolt@users.noreply.github.com>
2020-05-04 21:24:49 +0500

    Added Booles Formula in Numerical Intergration

---
+++ 3-Numerical Integration/Booles_Formula.m
@@ -0,0 +1,22 @@
+eq = input(" Enter the Equation F(x) \n",'s');
+f = str2func(['@(x)' eq]);
+
+a = input("Enter the lower limit \n");
+b = input("Enter the upper limit \n");
+
+n = 4; %Boole's Formula Divides The SubIntervals Into 4
+delta_x = (b-a)/n;
+x = a;
+calc = 0;
+for i=0:n
+  y = f(x);
+  if(i==0 || i==4)
+      calc = calc + 7*y;
+  elseif (i==1 || i==3)
+      calc = calc + 32*y;
+  else
+      calc = calc + 12*y;
+  end
+  x = x + delta_x;
+end
+Ans = (2/45)*delta_x*calc
