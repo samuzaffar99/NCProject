9d38740f52dc397d28b0fc9730dcfcf9eb1e65f4
samuzaffar99 <sam.muzaffar99@gmail.com>
2020-06-04 23:30:17 +0500

    removed chap 5, fnished bisection

--- 1-Solution of non linear equation/Bisection.m
+++ 1-Solution of non linear equation/Bisection.m
@@ -1,32 +1,66 @@
 fprintf("Bisection Method\n");
-exp=input("Enter expression: ",'s');
-f=inline(exp);
+N=1000;
+Lower=zeros(N);
+Upper=zeros(N);
+Mid=zeros(N);
+FLower=zeros(N);
+FUpper=zeros(N);
+FMid=zeros(N);
+Err=zeros(N);
 
-Lower=input("Enter Lower bound: ");
-Upper=input("Enter Upper bound: ");
+exp=input("Enter expression: ",'s');
+f=str2func(['@(x)' exp]);
+
+Lower(1)=input("Enter Lower bound: ");
+Upper(1)=input("Enter Upper bound: ");
 T=input("Enter the allowed error: ");
-while Lower>Upper || f(Lower)*f(Upper)<0
+
+while Lower(1)>Upper(1) || f(Lower(1))*f(Upper(1))>0
     fprintf("Incorrect Bounds!\n");
     Lower=input("Enter Lower bound: ");
     Upper=input("Enter Upper bound: ");
 end
-xnew(1)=0;
-for i=2:1000
-    Mid=(Lower+Upper)/2;
-    if f(Lower)*f(Mid)<0
-        Lower=Mid;
-    else
-        Upper=Mid;
-    end
-    if f(Upper)*f(Mid)<0
-        Upper=Mid;
-    else
-        Lower=Mid;
-    end
-xnew(i)=Mid;
-err(i)=abs((xnew(i)-xnew(i-1))/xnew(i));
-if err<T
-    break
+
+Err(1)=0;
+nitr=0;
+for i=1:N
+	fprintf("Iteration: %d\n",i);
+	Mid(i)=(Lower(i)+Upper(i))/2;
+	FMid(i)=f(Mid(i));
+	FLower(i)=f(Lower(i));
+	FUpper(i)=f(Upper(i));
+	if (f(Mid(i))==0)
+		fprintf("The required root of the equation is: %f\n",Mid(i));
+		nitr=i;
+		break
+	end
+	if(i~=1)
+		Err(i)=abs((Mid(i)-Mid(i-1)));
+		if Err(i)<T
+			nitr=i;
+			break
+		end
+	end
+	
+	
+	
+	if ((FLower(i)*FMid(i))<0)
+		Upper(i+1)=Mid(i);
+		Lower(i+1)=Lower(i);
+	else
+		Lower(i+1)=Mid(i);
+		Upper(i+1)=Upper(i);
+	end
 end
-end
-fprintf("The required root of the equation is: %f",num2str(Mid));
+
+fprintf("The required root of the equation is: %f\n",Mid(i));
+Lower=transpose(Lower(1:nitr));
+Upper=transpose(Upper(1:nitr));
+Mid=transpose(Mid(1:nitr));
+FLower=transpose(FLower(1:nitr));
+FUpper=transpose(FUpper(1:nitr));
+FMid=transpose(FMid(1:nitr));
+Err=transpose(Err(1:nitr));
+i=transpose(1:nitr);
+
+Table = table(i,Lower,Upper,Mid,FMid,Err,FUpper,FLower)
