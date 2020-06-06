45858d3265223d088420adc20e54ac4f57cbe08c
samuzaffar99 <sam.muzaffar99@gmail.com>
2020-06-06 18:59:14 +0500

    moved into folder, added introduction

--- 2-Code/2-Interpolation/NewtonsCentral.m
+++ 2-Code/2-Interpolation/NewtonsCentral.m
@@ -1,5 +1,7 @@
+fprintf("Central Divided Difference\n");
+
 n = input(" Enter the number of data sets(values) \n");
-k= n;
+k = n;
 delta_y = cell(n,n-1);
 for i=1:n
     for j=1:n-1
@@ -7,11 +9,12 @@ for i=1:n
     end
 end    
 for i=1:n
-    promptx = sprintf("Enter the value for x(%d) \n",i-1);
+	promptx = sprintf("Enter the value for x(%d) \n",i-1);
     x(i) = input(promptx);
     prompty = sprintf("Enter the value for y(%d) \n",i-1);
     y(i) = input(prompty);
 end
+
 val= input(" Enter the value to estimate result\n");
 h = x(2)-x(1);
 y0=(n+1)/2;
