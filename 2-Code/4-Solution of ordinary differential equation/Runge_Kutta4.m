f5d8b94182cb2bf84953521148fb09284cd5be95
megasxgigavolt <63855153+megasxgigavolt@users.noreply.github.com>
2020-06-06 22:28:49 +0500

    Tables(improved)ODE

--- 2-Code/4-Solution of ordinary differential equation/Runge_Kutta4.m
+++ 2-Code/4-Solution of ordinary differential equation/Runge_Kutta4.m
@@ -11,18 +11,28 @@ e = input("Enter the end of the range\n");
 
 h = input("Enter the value of h\n");
 i = input("Enter the initial value of y\n");
+
+n = (e-s)/h;
+x = zeros(1,n);
+y = zeros(1,n);
+y2 = zeros(1,n);
+error = zeros(1,n);
 yprev = i;
+itr = 1;
+
 clc
-fprintf('t \t\t\t\t Runge-Kutta \t y(t) \t\t\t Error\n');
 for t=s:h:e-h
     y0 = yprev;
     k1 = h*f(t,y0);
     k2 = h*f(t+(h/2),y0+(k1/2));
     k3 = h*f(t+(h/2),y0+(k2/2));
     k4 = h*f(t+h,y0+k3);
-    y = y0 + (1/6)*(k1+(2*k2)+(2*k3)+k4);
-    yprev = y;
-    y2 = f2(t+h,y0);
-    error = abs(y2-y);
-    fprintf('%f \t\t %f \t\t %f \t\t %f\n',t+h,y,y2,error);
+    y(itr) = y0 + (1/6)*(k1+(2*k2)+(2*k3)+k4);
+    yprev = y(itr);
+    y2(itr) = f2(t+h,y0);
+    error(itr) = abs(y2(itr)-y(itr));
+    x(itr) = t+h;
+    itr = itr + 1;
 end
+T = (array2table([x(:) y(:) y2(:) error(:)],...
+    'VariableNames',{'t','Runge-Kutta4','f(t)','Error'}))
